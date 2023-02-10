e9d8e7a0bfb20ce2e5f28e3636286bac60d451c3
Rsuraj143 <raudasuraj7@gmail.com>
2023-02-10 15:48:18 +0530

    Dashboad changes

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 import notification from "../../Images/h-notification.svg"
 import msg from "../../Images/h-msg.svg"
 import user from "../../Images/h-user.svg"
+import  Search  from "../../Images/search.svg"
 
 const Header = () => {
   return (
@@ -11,8 +12,9 @@ const Header = () => {
       <header class="header">
         <div class="header-main">
           <div class="search-box">
-            <i class="bx bx-search"></i>
+            {/* <i class="bx bx-search"></i> */}
             <input type="text" placeholder="Search..." />
+            <img src={Search}  alt="dashbord"/>
           </div>
           <ul class="icon-list">
             <li>
--- src/Components/SlideBar/SlideBar.js
+++ src/Components/SlideBar/SlideBar.js
@@ -8,6 +8,9 @@ import sales from "../../Images/sales.svg"
 import Products from "../../Images/Products.svg"
 import Audience from "../../Images/Audience.svg"
 import siderbarbtn from "../../Images/siderbar-btn.svg"
+import English from "../../Images/English.svg"
+import Logout from "../../Images/Logout.svg"
+import Settings from "../../Images/Settings.svg"
 const SlideBar = () => {
   return (
     <div class="sidebar active">
@@ -19,81 +22,100 @@ const SlideBar = () => {
         </div>
         <img src={siderbarbtn} id="btn"  alt="dashbord"/>
       </div>
-      <ul class="nav_list">
-        <li>
-          <Link to="#">
-          <div className="icons">
-              <img src={Liveview} alt="dashbord"/>
-          </div>
-
-            <span class="link_names">Live view</span>
-          </Link>
-          <span class="tooltip">Live view</span>
-        </li>
-        <li>
-          <Link to="#">
-      
-           <div className="icons">
-              <img src={dashbord} alt="dashbord"/>
-          </div>
-            <span class="link_names">Dashboard</span>
-          </Link>
-          <span class="tooltip">Dashboard</span>
-        </li>
-        <li>
-          <Link to="#">
-          <div className="icons">
-              <img src={DailyFlash} alt="dashbord"/>
-          </div>
-
-            <span class="link_names">Daily Flash</span>
-          </Link>
-          <span class="tooltip">Daily Flash</span>
-        </li>
-        <li>
-          <Link to="#">
-          <div className="icons">
-              <img src={sales} alt="dashbord"/>
-          </div>
-            <span class="link_names">Sales</span>
-          </Link>
-          <span class="tooltip">Sales</span>
-        </li>
-        <li>
-          <Link to="#">
-          <div className="icons">
-              <img src={Products} alt="dashbord"/>
-          </div>
-
-            <span class="link_names">Products</span>
-          </Link>
-          <span class="tooltip">Products</span>
-        </li>
-        <li>
-          <Link to="#">
-          <div className="icons">
-              <img src={Audience} alt="dashbord"/>
-          </div>
-
-            <span class="link_names">Audience</span>
-          </Link>
-          <span class="tooltip">Audience</span>
-        </li>
-      </ul>
-      <div class="profile_content">
-        <div class="profile">
-          <div class="profile_details">
-            <img
-              src="https://vz.cnwimg.com/wp-content/uploads/2014/01/alex.jpg?x86007"
-              alt=""
-            />
-            <div class="name_job">
-              <div class="name">Name</div>
-              <div class="job">Role</div>
+      <div className="sidebar-box">
+        <ul class="nav_list">
+          <li>
+            <Link to="#">
+            <div className="icons">
+                <img src={Liveview} alt="dashbord"/>
             </div>
-          </div>
-          <i class="bx bx-log-out" id="log_out"></i>
-        </div>
+
+              <span class="link_names">Live view</span>
+            </Link>
+            <span class="tooltip">Live view</span>
+          </li>
+          <li>
+            <Link to="#">
+        
+            <div className="icons">
+                <img src={dashbord} alt="dashbord"/>
+            </div>
+              <span class="link_names">Dashboard</span>
+            </Link>
+            <span class="tooltip">Dashboard</span>
+          </li>
+          <li>
+            <Link to="#">
+            <div className="icons">
+                <img src={DailyFlash} alt="dashbord"/>
+            </div>
+
+              <span class="link_names">Daily Flash</span>
+            </Link>
+            <span class="tooltip">Daily Flash</span>
+          </li>
+          <li>
+            <Link to="#">
+            <div className="icons">
+                <img src={sales} alt="dashbord"/>
+            </div>
+              <span class="link_names">Sales</span>
+            </Link>
+            <span class="tooltip">Sales</span>
+          </li>
+          <li>
+            <Link to="#">
+            <div className="icons">
+                <img src={Products} alt="dashbord"/>
+            </div>
+
+              <span class="link_names">Products</span>
+            </Link>
+            <span class="tooltip">Products</span>
+          </li>
+          <li>
+            <Link to="#">
+            <div className="icons">
+                <img src={Audience} alt="dashbord"/>
+            </div>
+
+              <span class="link_names">Audience</span>
+            </Link>
+            <span class="tooltip">Audience</span>
+          </li>
+        </ul>
+        <ul class="nav_list">
+          <li>
+            <Link to="#">
+            <div className="icons lang">
+                <img src={English} alt="dashbord"/>
+            </div>
+
+              <span class="link_names">English</span>
+            </Link>
+            <span class="tooltip">English</span>
+          </li>
+          <li>
+            <Link to="#">
+        
+            <div className="icons">
+                <img src={Settings} alt="dashbord"/>
+            </div>
+              <span class="link_names">Settings</span>
+            </Link>
+            <span class="tooltip">Settings</span>
+          </li>
+          <li>
+            <Link to="#">
+            <div className="icons">
+                <img src={Logout} alt="dashbord"/>
+            </div>
+
+              <span class="link_names">Logout</span>
+            </Link>
+            <span class="tooltip">Logout</span>
+          </li>
+        </ul>
       </div>
     </div>
   );
