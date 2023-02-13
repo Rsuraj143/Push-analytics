1a826cb6159553896cb9600be4d6d8655494e4c8
Rsuraj143 <raudasuraj4@gmail.com>
2023-02-13 09:26:30 +0530

    slider is working smooth

--- src/Components/SlideBar/SlideBar.js
+++ src/Components/SlideBar/SlideBar.js
@@ -1,34 +1,51 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./SlideBar.css";
 import { Link } from "react-router-dom";
-import Liveview from "../../Images/Live-view.svg"
-import dashbord from "../../Images/Dashboard.svg"
-import DailyFlash from "../../Images/Daily-Flash.svg"
-import sales from "../../Images/sales.svg"
-import Products from "../../Images/Products.svg"
-import Audience from "../../Images/Audience.svg"
-import siderbarbtn from "../../Images/siderbar-btn.svg"
-import English from "../../Images/English.svg"
-import Logout from "../../Images/Logout.svg"
-import Settings from "../../Images/Settings.svg"
+import Liveview from "../../Images/Live-view.svg";
+import dashbord from "../../Images/Dashboard.svg";
+import DailyFlash from "../../Images/Daily-Flash.svg";
+import sales from "../../Images/sales.svg";
+import Products from "../../Images/Products.svg";
+import Audience from "../../Images/Audience.svg";
+import siderbarbtn from "../../Images/siderbar-btn.svg";
+import English from "../../Images/English.svg";
+import Logout from "../../Images/Logout.svg";
+import Settings from "../../Images/Settings.svg";
+
 const SlideBar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+
+  const btnref = useRef();
+  const handleSlide = () => {
+    btnref.current.click();
+    setClick(!click)
+  };
+
   return (
-    <div class="sidebar active">
+    <div class={click ? "sidebar" : "sidebar active"}>
       <div class="logo_content">
         <div class="logo">
           <div class="logoname" style={{ marginleft: "5px" }}>
             Push
           </div>
         </div>
-        <img src={siderbarbtn} id="btn"  alt="dashbord"/>
+        <img
+          src={siderbarbtn}
+          id="btn"
+          ref={btnref}
+          onClick={() => handleSlide()}
+          alt="dashbord"
+        />
       </div>
+
       <div className="sidebar-box">
         <ul class="nav_list">
           <li>
             <Link to="#">
-            <div className="icons">
-                <img src={Liveview} alt="dashbord"/>
-            </div>
+              <div className="icons">
+                <img src={Liveview} alt="dashbord" />
+              </div>
 
               <span class="link_names">Live view</span>
             </Link>
@@ -36,19 +53,18 @@ const SlideBar = () => {
           </li>
           <li>
             <Link to="#">
-        
-            <div className="icons">
-                <img src={dashbord} alt="dashbord"/>
-            </div>
+              <div className="icons">
+                <img src={dashbord} alt="dashbord" />
+              </div>
               <span class="link_names">Dashboard</span>
             </Link>
             <span class="tooltip">Dashboard</span>
           </li>
           <li>
             <Link to="#">
-            <div className="icons">
-                <img src={DailyFlash} alt="dashbord"/>
-            </div>
+              <div className="icons">
+                <img src={DailyFlash} alt="dashbord" />
+              </div>
 
               <span class="link_names">Daily Flash</span>
             </Link>
@@ -56,18 +72,18 @@ const SlideBar = () => {
           </li>
           <li>
             <Link to="#">
-            <div className="icons">
-                <img src={sales} alt="dashbord"/>
-            </div>
+              <div className="icons">
+                <img src={sales} alt="dashbord" />
+              </div>
               <span class="link_names">Sales</span>
             </Link>
             <span class="tooltip">Sales</span>
           </li>
           <li>
             <Link to="#">
-            <div className="icons">
-                <img src={Products} alt="dashbord"/>
-            </div>
+              <div className="icons">
+                <img src={Products} alt="dashbord" />
+              </div>
 
               <span class="link_names">Products</span>
             </Link>
@@ -75,9 +91,9 @@ const SlideBar = () => {
           </li>
           <li>
             <Link to="#">
-            <div className="icons">
-                <img src={Audience} alt="dashbord"/>
-            </div>
+              <div className="icons">
+                <img src={Audience} alt="dashbord" />
+              </div>
 
               <span class="link_names">Audience</span>
             </Link>
@@ -87,9 +103,9 @@ const SlideBar = () => {
         <ul class="nav_list">
           <li>
             <Link to="#">
-            <div className="icons lang">
-                <img src={English} alt="dashbord"/>
-            </div>
+              <div className="icons lang">
+                <img src={English} alt="dashbord" />
+              </div>
 
               <span class="link_names">English</span>
             </Link>
@@ -97,19 +113,18 @@ const SlideBar = () => {
           </li>
           <li>
             <Link to="#">
-        
-            <div className="icons">
-                <img src={Settings} alt="dashbord"/>
-            </div>
+              <div className="icons">
+                <img src={Settings} alt="dashbord" />
+              </div>
               <span class="link_names">Settings</span>
             </Link>
             <span class="tooltip">Settings</span>
           </li>
           <li>
             <Link to="#">
-            <div className="icons">
-                <img src={Logout} alt="dashbord"/>
-            </div>
+              <div className="icons">
+                <img src={Logout} alt="dashbord" />
+              </div>
 
               <span class="link_names">Logout</span>
             </Link>
